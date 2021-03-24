e94629d5497c024a3184dd1be50aa770b7be3af8
Hyejin Lee <hichjay@gmail.com>
2021-03-24 17:46:42 +0900

    feat: Community.vue 상단 타이틀 수정

--- frontend/halaltimes/src/pages/community/Community.vue
+++ frontend/halaltimes/src/pages/community/Community.vue
@@ -1,10 +1,34 @@
 <template>
-    <span>Community</span>
+  <div
+    id="bg"
+    class="G-market-sans font-color-black-400 w-2/3 mx-auto px-10 py-6"
+  >
+    <section class="flex justify-content-center mt-4 mb-6">
+      <span
+        class="text-5xl mx-10 w-full text-center py-1 mt-4 mb-2 font-outline work-sans border-line"
+        >Community</span
+      >
+    </section>
+    <section class="flex">
+      <article class="w-1/2"></article>
+      <article class="flex flex-col w-1/2 h-auto mx-2"></article>
+    </section>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-       return {}; 
-    },
-}
+  data() {
+    return {};
+  },
+};
 </script>
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+
+#bg {
+  background-color: #f4f2ea;
+}
+</style>
