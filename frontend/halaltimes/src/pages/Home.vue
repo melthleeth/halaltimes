ff50068ca9abc87afedabab7c11dea0aa751f4ee
JH_Joo <wjdgnsx94@naver.com>
2021-03-24 14:32:35 +0900

    feat: S04P23A206-42 KakaoMap.vue Prop Data 설정

--- frontend/halaltimes/src/pages/Home.vue
+++ frontend/halaltimes/src/pages/Home.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <span class="text-lg">HOME</span>
+        <kakao-map/>
     </div>
 </template>
 <script>
+import KakaoMap from '@/components/layout/KakaoMap.vue'
 export default {
+  components: { KakaoMap },
     
 }
 </script>
