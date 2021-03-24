3a96ae78f7d63a6561b955dd41b371791a582c26
gwak <gjw0407@gmail.com>
2021-03-24 17:59:28 +0900

    fix: S04P23A206-35 로그인 logic BE 연결

---
+++ frontend/halaltimes/src/components/layout/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="G-market-sans font-color-black-400 flex w-full">
+        <span class="text-sm justify-content-center mx-auto mt-4 mb-6">2021 ⓒ 할랄 타임즈 All rights reserved</span>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
