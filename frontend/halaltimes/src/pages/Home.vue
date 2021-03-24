d04aa3f21c1fb0c0000f850e19bc3f2f20c320f6
Hyejin Lee <hichjay@gmail.com>
2021-03-24 17:45:37 +0900

    update: Home.vue

--- frontend/halaltimes/src/pages/Home.vue
+++ frontend/halaltimes/src/pages/Home.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
-        <span class="text-lg">HOME</span>
-        <kakao-map/>
-    </div>
+  <div
+    id="bg"
+    class="G-market-sans font-color-black-400 w-2/3 mx-auto px-10 py-6"
+  >
+    <section class="flex justify-content-center mt-4 mb-6">
+      <span
+        class="text-5xl mx-10 w-full text-center py-1 mt-4 mb-2 font-outline work-sans border-line"
+        >Halal Times</span
+      >
+    </section>
+    <section class="flex">
+      <article class="w-1/2"></article>
+      <article class="flex flex-col w-1/2 h-auto mx-2"></article>
+    </section>
+  </div>
 </template>
 <script>
-import KakaoMap from '@/components/layout/KakaoMap.vue'
+// import KakaoMap from "@/components/layout/KakaoMap.vue";
 export default {
-  components: { KakaoMap },
-    
-}
+//   components: { KakaoMap },
+  data() {
+    return {};
+  },
+};
 </script>
 <style scoped>
-body {
-    min-height: 800px;
+img {
+  width: 100%;
+  height: 100%;
+}
+
+#bg {
+  background-color: #f4f2ea;
 }
 </style>
