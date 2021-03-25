acb423685a5f6d51cffa8ac8d67a5f1642a54b3f
Hyejin Lee <hichjay@gmail.com>
2021-03-26 03:24:56 +0900

    feat: spinner 파일 생성

--- frontend/halal-times/src/main.js
+++ frontend/halal-times/src/main.js
@@ -7,6 +7,8 @@ import "./assets/styles/index.css";
 import BaseButton from "./components/ui/BaseButton.vue";
 import BaseDialog from "./components/ui/BaseDialog.vue";
 import BaseTitle from "./components/ui/BaseTitle.vue";
+import BaseTitleSmall from "./components/ui/BaseTitleSmall.vue";
+import BaseSpinner from "./components/ui/BaseSpinner.vue";
 
 const app = createApp(App);
 
@@ -16,5 +18,7 @@ app.use(store);
 app.component("base-button", BaseButton);
 app.component("base-dialog", BaseDialog);
 app.component("base-title", BaseTitle);
+app.component("base-title-small", BaseTitleSmall);
+app.component("base-spinner", BaseSpinner);
 
 app.mount("#app");
