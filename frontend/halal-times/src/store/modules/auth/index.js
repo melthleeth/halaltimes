2389aa17ee243ca05d39d6b7e112d0e52332385f
Hyejin Lee <hichjay@gmail.com>
2021-03-30 15:47:17 +0900

    feat: S04P23A206-81: 로그인/로그아웃 모듈화

--- frontend/halal-times/src/store/modules/auth/index.js
+++ frontend/halal-times/src/store/modules/auth/index.js
@@ -3,11 +3,27 @@ import actions from './actions.js';
 import getters from './getters.js';
 
 export default {
-    state() {
-      return {
-      };
-    },
-    mutations,
-    actions,
-    getters,
-  };
+  state() {
+    return {
+      // articles: require('@/data/articles.json'),
+      drawer: false,
+      items: [
+        {
+          text: '홈으로',
+          href: ''
+        }
+        // {
+        //   text: '게시판',
+        //   href: '/board',
+        // },
+      ],
+      accessToken: null,
+      userEmail: '',
+      userName: '',
+      role: ''
+    };
+  },
+  mutations,
+  actions,
+  getters
+};
