c7abb57a2c760e22b9666f6283304dc464615b27
Hyejin Lee <hichjay@gmail.com>
2021-04-09 03:14:34 +0900

    fix: 중복 코드 오류 수정

--- exec/frontend/halal-times/src/store/modules/auth/mutations.js
+++ exec/frontend/halal-times/src/store/modules/auth/mutations.js
@@ -6,27 +6,6 @@ export default {
     state.userEmail = payload['user-email'];
     state.userName = payload['user-name'];
     state.role = payload['role'];
-  },
-  LOGOUT(state) {
-    state.accessToken = null;
-    state.userEmail = '';
-    state.userName = '';
-    state.role = '';
-  },
-  REGIST(state, payload) {
-    state.accessToken = payload['auth-token'];
-    state.userEmail = payload['user-email'];
-    state.userName = payload['user-name'];
-  }
-};
-export default {
-  setDrawer: (state, payload) => (state.drawer = payload),
-  toggleDrawer: state => (state.drawer = !state.drawer),
-  LOGIN(state, payload) {
-    state.accessToken = payload['auth-token'];
-    state.userEmail = payload['user-email'];
-    state.userName = payload['user-name'];
-    state.role = payload['role'];
     state.profileImage = payload['profile_image'] === null ? 'https://halaltimesbucket.s3.ap-northeast-2.amazonaws.com/%ED%84%B0%EB%B2%88.png' : payload['profile_image'];
   },
   LOGOUT(state) {
